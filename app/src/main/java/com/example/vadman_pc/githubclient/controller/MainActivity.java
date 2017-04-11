aee7accd24a527d3bd3ff569a28b26f71b401936
Vadym Moiseyenko <vadym.moiseyenko@gmail.com>
2017-04-11 10:46:52 +0300

    revert all fragment new begining

--- app/src/main/java/com/example/vadman_pc/githubclient/controller/MainActivity.java
+++ app/src/main/java/com/example/vadman_pc/githubclient/controller/MainActivity.java
@@ -4,16 +4,11 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.*;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -69,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
 
         initViews();
 
-        initFrgments();
         Log.d("VadmanLog", "swipecontainer");
         swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
         swipeContainer.setColorSchemeResources(android.R.color.holo_orange_dark);
@@ -87,23 +81,6 @@ public class MainActivity extends AppCompatActivity {
         loadJSONfromFirstPage();
     }
 
-    private void initFrgments() {
-        Log.d("VadmanLog", "begine init frag");
-        ViewPager viewPager = (ViewPager) findViewById(R.id.view_page);
-        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), MainActivity.this) {
-        };
-        viewPager.setAdapter(pagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(viewPager);
-
-        for (int i = 0; i < tabLayout.getTabCount(); i++) {
-            TabLayout.Tab tab = tabLayout.getTabAt(i);
-            tab.setCustomView(pagerAdapter.getTabView(i));
-        }
-        Log.d("VadmanLog", "finish init frag");
-
-}
 
     private void initViews() {
         Log.d("VadmanLog", "begine init Views");
@@ -282,48 +259,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    class PagerAdapter extends FragmentPagerAdapter {
-        String tabTitles[] = new String[]{"First Tab", "Second Tab"};
-        Context context;
 
-        public PagerAdapter(android.support.v4.app.FragmentManager supportFragmentManager, MainActivity context) {
-            super(supportFragmentManager);
-            this.context = context;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-
-            switch (position) {
-                case 0:
-                    return new BlankFragment();
-
-                case 1:
-                    return new BlankFragment();
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            return tabTitles.length;
-        }
-
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return tabTitles[position];
-        }
-
-        public View getTabView(int position) {
-            View tab = LayoutInflater.from(MainActivity.this).inflate(R.layout.custom_tab, null);
-            TextView tv = (TextView) tab.findViewById(R.id.custom_text);
-            tv.setText(tabTitles[position]);
-            return tab;
-        }
-    }
-
-    public static Context getAppContext() {
-        return MainActivity.context;
-    }
 }
